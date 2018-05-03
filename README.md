4ed670e58aecca0a1a9c84cc6b8c79db5a275893
AlexMarshall011 <onlyalex1995@gmail.com>
2018-05-03 17:06:40 +0800

    update README

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # pytorch-polygon-rnn
 Pytorch implementation of Polygon-RNN
+
+
+### Download and Preprocess Data
+
+1. Download data from [CityScapes](https://www.cityscapes-dataset.com/downloads/), organize
+the image files and annotation json files as follows:
+
+
+
+```
+img
+├── train
+│   ├── cityname1
+│   │   ├── pic.png
+│   │   ├── ...
+│   ├── cityname2
+│   │   ├── pic.png
+│   │   ├── ...
+├── val
+│   ├── cityname
+│   │   ├── pic.png
+│   │   ├── ...
+├── test
+│   ├── cityname
+│   │   ├── pic.png
+│   │   ├── ...
+```
+
+```
+label
+├── train
+│   ├── cityname1
+│   │   ├── annotation.json
+│   │   ├── ...
+│   ├── cityname2
+│   │   ├── annotation.json
+│   │   ├── ...
+├── val
+│   ├── cityname
+│   │   ├── annotation.json
+│   │   ├── ...
+├── test
+│   ├── cityname
+│   │   ├── annotation.json
+│   │   ├── ...
+```
+
+The png files and the json files should have corresponding same name.
+
+Execute the following command to make directories for new data:
+```
+mkdir -p new_img/(train/val/test)
+mkdir -p new_label/(train/val/test)
+```
+
+2. Run the following command to generate data for train/validation/test.
+```
+python generate_data.py --data train/val/test
+```
+
+3. Run the following command to train.
+```
+python train.py --gpu_id 0 1 2 --batch_size 4 --pretrained False --lr 0.001
+```
--- generate_data.py
+++ generate_data.py
@@ -2,20 +2,20 @@ from glob import glob
 import json
 import cv2
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(description='manual to this script')
+parser.add_argument('--data', type=str, default="train")
+args = parser.parse_args()
 
 
-
-selected_classes = ['car']
-dataset = 'trainval'
+selected_classes = ['person', 'car', 'truck', 'bicycle', 'motorcycle', 'rider', 'bus', 'train']
+dataset = args.data
+files = glob('img/'+dataset+'/*/*.png')
 count = 0
 max_count = 0
-num = 48433
 ind = 0
-len_s = 80
-img_array = np.zeros([num, 3, 224, 224])
-label_array = np.zeros([num, len_s, 28 * 28 + 1])
-label_index_array = np.zeros([num, len_s])
-files = glob('img/'+dataset+'/*/*.png')
+
 for file in files:
     json_file = 'label' + file[3:-15] + 'gtFine_polygons.json'
     json_object = json.load(open(json_file))
@@ -23,7 +23,6 @@ for file in files:
     w = json_object['imgWidth']
     objects = json_object['objects']
     for obj in objects:
-        #         if obj['label'] == 'truck':
         if obj['label'] in selected_classes:
             min_c = np.min(np.array(obj['polygon']), axis=0)
             max_c = np.max(np.array(obj['polygon']), axis=0)
@@ -42,56 +41,27 @@ for file in files:
             I = cv2.imread(file)
             I_obj = I[min_row:max_row, min_col:max_col, :]
             I_obj_new = cv2.resize(I_obj, (224, 224))
-            cv2.imwrite('car_img/'+dataset+'/'+ str(count) + '.png', I_obj_new)
+            cv2.imwrite('new_img/'+dataset+'/'+ str(count) + '.png', I_obj_new)
             I_obj = np.rollaxis(I_obj_new, 2)
-            img_array[count, :, :, :] = I_obj
             point_count = 0
             last_index_a = -1
             last_index_b = -1
             dic = {}
             dic['polygon'] = []
-            #             len_p = len(obj['polygon'])
             for p_count, points in enumerate(obj['polygon']):
                 index_a = (points[0] - min_col) * scale_w
                 index_b = (points[1] - min_row) * scale_h
                 index_a = np.maximum(0, np.minimum(223, index_a))
                 index_b = np.maximum(0, np.minimum(223, index_b))
                 dic['polygon'].append([index_a, index_b])
-            with open('car_label/' +dataset+'/' + str(count) + '.json', 'w') as jsonfile:
+            with open('new_label/' +dataset+'/' + str(count) + '.json', 'w') as jsonfile:
                 json.dump(dic, jsonfile)
-            #                 index = index_b*28 + index_a
-            #                 if abs(index_a-last_index_a)<=1 and abs(index_b-last_index_b)<=1:
-            #                     continue
-            #                 last_index_a = index_a
-            #                 last_index_b = index_b
-            #                 label_array[count,point_count,index]=1
-            #                 label_index_array[count,point_count]=index
-            #                 point_count += 1
-            #             for kkk in range(point_count,len_s):
-            #                 index = 28*28
-            #                 label_array[count,kkk,index]=1
-            #                 label_index_array[count,kkk]=index
-            #             label_array[count,point_count,28*28]=1
-
-            #             if max_count < point_count:
-            #                 max_count = point_count
             count += 1
-    #             plt.imshow(I[min_c[1]:max_c[1],min_c[0]:max_c[0],:])
-    #             for points in obj['polygon']:
-    #                 plt.scatter(points[0]-min_c[0],points[1]-min_c[1])
-    #             plt.show()
 
-    #             plt.imshow(I_obj_new)
-    #             for points in obj['polygon']:
-    #                 plt.imshow(I_obj_new)
-    #                 plt.scatter((points[0]-min_col)*scale_w,(points[1]-min_row)*scale_h)
-    #                 plt.show()
     if (count / 10000 >= ind):
         ind += 1
         print(count)
 
-#     if(count>100):
-#         break
+
 print(count)
 print(max_count)
-# print(files[3][3:-15])
